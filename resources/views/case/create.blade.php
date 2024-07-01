9a5a823bd66449b9f1da5d25aae328c46899d25e
TayyabAslam123 <tayyabaslam771@gmail.com>
2024-07-02 03:24:22 +0500

    booking form

--- resources/views/case/create.blade.php
+++ resources/views/case/create.blade.php
@@ -203,18 +203,128 @@
                                             <!--Priority  -- START -->
                                             <h3>Priority</h3>
                                             <section>
+                                            <h3 class="mt-3">Do you leave more parts ?</h3>
+                                                <br>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value="battery">
+                                                    <label class="form-check-label" for="battery">
+                                                        <h5>Standard</h5>
+                                                    </label>
+                                                </div>
+
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value="charger">
+                                                    <label class="form-check-label" for="charger">
+                                                        <h5>Higher (+45Eur) </h5>
+                                                    </label>
+                                                </div>
+
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value="other-parts">
+                                                    <label class="form-check-label" for="other-parts">
+                                                        <h5>Highest (+70Eur)</h5>
+                                                    </label>
+                                                </div>
                                             </section>
                                             <!--Priority -- START -->
 
                                             <!--Location  -- START -->
                                             <h3>Location</h3>
                                             <section>
+                                            <div class="card">
+                                    <div class="card-body">
+        
+                                       <h3 class="mt-3">Select Service Location</h3>
+                                        <br>
+                                        <!-- Nav tabs -->
+                                        <ul class="nav nav-pills nav-justified" role="tablist">
+                                            <li class="nav-item waves-effect waves-light" role="presentation">
+                                                <a class="nav-link active" data-bs-toggle="tab" href="#home-1" role="tab" aria-selected="true">
+                                                    <span class="d-block d-sm-none"><i class="fas fa-home"></i></span>
+                                                    <span class="d-none d-sm-block">Deliver To Office</span> 
+                                                </a>
+                                            </li>
+                                            <li class="nav-item waves-effect waves-light" role="presentation">
+                                                <a class="nav-link" data-bs-toggle="tab" href="#profile-1" role="tab" aria-selected="false" tabindex="-1">
+                                                    <span class="d-block d-sm-none"><i class="far fa-user"></i></span>
+                                                    <span class="d-none d-sm-block">I Will Send To Office</span> 
+                                                </a>
+                                            </li>
+                                            <li class="nav-item waves-effect waves-light" role="presentation">
+                                                <a class="nav-link" data-bs-toggle="tab" href="#messages-1" role="tab" aria-selected="false" tabindex="-1">
+                                                    <span class="d-block d-sm-none"><i class="far fa-envelope"></i></span>
+                                                    <span class="d-none d-sm-block">Invite Engineer To My Home</span>   
+                                                </a>
+                                            </li>
+                                         
+                                        </ul>
+        
+                                        <!-- Tab panes -->
+                                        <div class="tab-content p-3 text-muted">
+                                            <div class="tab-pane active show" id="home-1" role="tabpanel">
+                                                <p class="mb-0">
+                                                    Raw denim you probably haven't heard of them jean shorts Austin.
+                                                    Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
+                                                    cliche tempor, williamsburg carles vegan helvetica. Reprehenderit
+                                                    butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi,
+                                                    qui irure terry richardson ex squid. Aliquip placeat salvia cillum
+                                                    iphone. Seitan aliquip quis cardigan american apparel, butcher
+                                                    voluptate nisi qui.
+                                                </p>
+                                            </div>
+                                            <div class="tab-pane" id="profile-1" role="tabpanel">
+                                                <p class="mb-0">
+                                                    Food truck fixie locavore, accusamus mcsweeney's marfa nulla
+                                                    single-origin coffee squid. Exercitation +1 labore velit, blog
+                                                    sartorial PBR leggings next level wes anderson artisan four loko
+                                                    farm-to-table craft beer twee. Qui photo booth letterpress,
+                                                    commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
+                                                    vinyl cillum PBR. Homo nostrud organic, assumenda labore
+                                                    aesthetic magna 8-bit.
+                                                </p>
+                                            </div>
+                                            <div class="tab-pane" id="messages-1" role="tabpanel">
+                                                <p class="mb-0">
+                                                    Etsy mixtape wayfarers, ethical wes anderson tofu before they
+                                                    sold out mcsweeney's organic lomo retro fanny pack lo-fi
+                                                    farm-to-table readymade. Messenger bag gentrify pitchfork
+                                                    tattooed craft beer, iphone skateboard locavore carles etsy
+                                                    salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
+                                                    Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh
+                                                    mi whatever gluten-free.
+                                                </p>
+                                            </div>
+                                            <div class="tab-pane" id="settings-1" role="tabpanel">
+                                                <p class="mb-0">
+                                                    Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
+                                                    art party before they sold out master cleanse gluten-free squid
+                                                    scenester freegan cosby sweater. Fanny pack portland seitan DIY,
+                                                    art party locavore wolf cliche high life echo park Austin. Cred
+                                                    vinyl keffiyeh DIY salvia PBR, banh mi before they sold out
+                                                    farm-to-table.
+                                                </p>
+                                            </div>
+                                        </div>
+        
+                                    </div>
+                                </div>
                                             </section>
                                             <!--Location -- START -->
 
                                             <!--Confirmation  -- START -->
                                             <h3>Confirmation</h3>
                                             <section>
+                                              <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value="battery">
+                                                    <label class="form-check-label" for="battery">
+                                                        <h5>I read and agree with terms of service</h5>
+                                                    </label>
+                                                </div>
+
+                                                <div class="d-grid gap-2">
+                                            <button type="button" class="btn btn-primary btn-lg waves-effect waves-light">SUBMIT</button>
+                                            
+                                        </div>
                                             </section>
                                             <!--Confirmation -- START -->
 
