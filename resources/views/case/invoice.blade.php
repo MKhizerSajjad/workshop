eeba2a7f03ec467c7600a677b78813a72a70e0d1
Khizer Sajjad <khizer.sajjad@sixlogics.com>
2025-01-10 18:41:08 +0500

    update

--- resources/views/case/invoice.blade.php
+++ resources/views/case/invoice.blade.php
@@ -8,13 +8,13 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Invoice</h4>
+                        <h4 class="mb-sm-0 font-size-18">Case Report</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="">Case</a></li>
                                 <li class="mx-1"><a href="javascript: void(0);"> > </a></li>
-                                <li class="breadcrumb-item active">Invoice</li>
+                                <li class="breadcrumb-item active">Case Report</li>
                             </ol>
                         </div>
 
