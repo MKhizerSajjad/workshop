3aaee18532c06f67597a0a15f2d9268f4be838cf
Khizer Sajjad <khizer.sajjad@sixlogics.com>
2025-01-20 11:44:27 +0500

    add priority amount migration

--- database/migrations/2024_07_07_184217_create_tasks_table.php
+++ database/migrations/2024_07_07_184217_create_tasks_table.php
@@ -31,6 +31,7 @@ return new class extends Migration
             $table->longText('additional_info')->nullable()->comment('additional information about item');
             $table->longText('problem_description')->nullable()->comment('Description of the problem / failure');
             $table->bigInteger('priority_id')->unsigned()->nullable();
+            $table->decimal('priority_amount', 10, 2)->nullable();
             $table->boolean('services_location')->default(1)->comment('where from service get');
             $table->boolean('inspection_diagnose')->default(1)->comment('Inspection and diagnostics fix charges i.e: 35Eur');
             $table->decimal('inspection_diagnose_amount', 10, 2)->nullable();
